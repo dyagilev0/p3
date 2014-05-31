92aeeab3ca95db3d5e2d796412ad6ed8cdc037ba
vana0 <vana0@vana0-R530-R730-P530.(none)>
2014-06-01 01:55:36 +0400

    try to solve with object

--- p3.js
+++ p3.js
@@ -1,55 +1,71 @@
 var fs = require('fs');
 var g = [];
+var vertices = {};
 var file = 'kargerMinCut.txt';
 fs.readFile('test.txt', 'utf-8', function(err, data) {
 	if (err) throw err;
-	g = data.split('\r\n').map(splitT);
-	while (g.length > 2) {
-		g = cut(g, Math.floor(Math.random() * g.length));
+	g = data.split('\n');
+	for (var i = 1; i <= g.length; i++) {
+		vertices[i] = {
+			v: i
+		};
 	}
-	console.log(g);
+	console.log(vertices);
+	g = g.map(linked);
+
+	while (g.length > 2) {
+		g = cut(g);
+	}
+	for (var i = 0; i< g.length; i++) {
+		console.log(g[i].head,g[i].tail);
+	}
 });
 
-function splitT(a) {
-	return sliceLast(a.split('\t'));
+function linked(a) {
+	var m = a.split('\t');
+	var result = {
+		head: vertices[m[0]],
+		tail: []
+	};
+	for (var i = 1; i < m.length - 1; i++) {
+		result.tail.push(vertices[m[i]]);
+	}
+	return result;
 }
 
-function sliceLast(a) {
-	var b = a.pop();
-	return [a.shift(), a];
-}
-
-var cut = function(g, k) {
-	var ak = g[k][0],
-		line = [],
-		unicLine = [],
-		result = [],
-		bl = g[k][1][Math.floor(Math.random() * g[k][1].length)];
-	var bi;
-
-	for (var i = 0; i < g.length; i++) {
-		if (g[i][0] === bl) {
-			bi = i;
+var cut = function(arr) {
+	var i = Math.random() * arr.length << 0;
+	var j = Math.random() * arr[i].tail.length << 0;
+	var a = vertices[arr[i].head.v];
+	var b = vertices[arr[i].tail[j].v];
+	var bi = null;
+	var result = [];
+	for (var k = 0; k < arr.length; k++) {
+		if (arr[k].head === b) {
+			bi = k;
 			break;
 		}
 	}
 
-	for (var i = 0; i < g.length; i++) {
-		if (i !== k && i !== bi) {
-			line = g[i][1];
-			for (var j = 0; j < line.length; j++) {
-				if (line[j] === bl) line[j] = ak;
-			}
-			result.push([g[i][0], line]);
-		}
+	console.log(i, j, a, b, bi);
+	var temp = [];
+	for(var k = 0; k < arr[i].tail.length; k++) {
+		if (arr[i].tail[k] !== b) temp.push(arr[i].tail[k]);
+	}
+	arr[i].tail = temp;
+
+	temp = [];
+	for(var k = 0; k < arr[bi].tail.length; k++) {
+		if (arr[bi].tail[k] !== a) temp.push(arr[bi].tail[k]);
+	}
+	arr[bi].tail = temp;
+
+	arr[i].tail.concat(arr[bi].tail);
+
+	for (var k = 0; k < arr.length; k++) {
+		if (k !== bi) result.push(arr[k]);
 	}
 
-	line = g[k][1].concat(g[bi][1]);
-	for (var j = 0; j < line.length; j++) {
-		if (line[j] === bl) line[j] = ak;
-	}
-
-	result.push([g[k][0], line]);
-
+	vertices[b.v].v = vertices[a.v].v;	
 	return result;
 }
